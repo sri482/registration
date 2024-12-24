afa8505cab78ce5a57afb979609631b56e499174
Sriram <123171568+sri482@users.noreply.github.com>
2024-12-24 16:58:21 +0530

    Delete sql.sql

--- sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE candidate_registration;
-CREATE TABLE candidates (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    phone VARCHAR(15),
-    dob DATE,
-    resume_path VARCHAR(255)
-);
