4095a7006a368aba282e55d9806c0168ae4eb7f1
Sriram <123171568+sri482@users.noreply.github.com>
2024-12-24 15:36:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def submit_form():
 
     # Insert data into MySQL
     query = "INSERT INTO candidates (name, email, phone) VALUES (%s, %s, %s)"
-    values = (పేరు, email, phone)
+    values = (పేరు, email, ఫోన్ నెం)
     cursor.execute(query, values)
     db.commit()
 
